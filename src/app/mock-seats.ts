56c676412e221582670e0341cff7ebaed66c52ac
ELVIN PANTOWIDJOYO <U055585@bca>
2020-01-30 19:12:44 +0700

    minor fixes

--- src/app/mock-seats.ts
+++ src/app/mock-seats.ts
@@ -1,11 +1,11 @@
 import { Seat } from "./seat";
 
 export const SEATS: Seat[] = [
-    { id: 1, name: 'A1', x: 10, y: 10, isVacant: true, isCubicle: true },
-    { id: 2, name: 'A2', x: 20, y: 10, isVacant: true, isCubicle: true },
-    { id: 3, name: 'A3', x: 30, y: 10, isVacant: true, isCubicle: true },
-    { id: 4, name: 'A4', x: 40, y: 10, isVacant: true, isCubicle: true },
-    { id: 5, name: 'A5', x: 50, y: 10, isVacant: true, isCubicle: true },
-    { id: 6, name: 'A6', x: 60, y: 10, isVacant: true, isCubicle: true },
-    { id: 7, name: 'A7', x: 70, y: 10, isVacant: true, isCubicle: true }
+    { id: 1, name: 'A1', x: 10, y: 10, isVacant: true, isCubicle: true, empId: null },
+    { id: 2, name: 'A2', x: 20, y: 10, isVacant: true, isCubicle: true, empId: null },
+    { id: 3, name: 'A3', x: 30, y: 10, isVacant: true, isCubicle: true, empId: null },
+    { id: 4, name: 'A4', x: 40, y: 10, isVacant: true, isCubicle: true, empId: null },
+    { id: 5, name: 'A5', x: 50, y: 10, isVacant: true, isCubicle: true, empId: null },
+    { id: 6, name: 'A6', x: 60, y: 10, isVacant: true, isCubicle: true, empId: null },
+    { id: 7, name: 'A7', x: 70, y: 10, isVacant: true, isCubicle: true, empId: null }
 ]
